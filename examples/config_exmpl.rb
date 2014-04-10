884c152f63ed1592de302394ece872d040e1a39d
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-04-10 08:13:11 +0200

    update config example

--- examples/config_exmpl.rb
+++ examples/config_exmpl.rb
@@ -1,9 +1,10 @@
 require 'tmp'
 
-TMP::Config.folder_path File.expand_path(File.join(File.dirname(__FILE__),'tmp_folder'))
+TMP.folder_path File.expand_path(File.join(File.dirname(__FILE__),'tmp_folder'))
 
 # or you can use syntax sugar!
 tmp.hello = { hello: 'world'}
+
 # defined variable
 puts tmp.hello #> { hello: 'world'}
 
